d5409645834084eef0b7953b35fc5d2e96f4ddaf
Joe Olson <technology@nododos.com>
2020-08-25 11:11:01 -0500

    fixed typo

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# Can configure at Docker build time, if needed.
 exec pipenv run python brute_force_wc.py --bucket testupload01 --region us-east-1 --inputKey input.txt --outputKey output.txt
